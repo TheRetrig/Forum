84de6c5c34aa97c87af04abc37d22ce64c065522
auralshin <anshulspartan141@gmail.com>
2020-11-21 16:46:28 +0530

    sample auth file

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+// export default async function ({ app, store, error, redirect }) {
+//     // If the user is not authenticated
+//    
+//     if (""condition"") {
+//       redirect('/login')
+//     } else {
+//       redirect('/')
+//     }
+//   }
+  
