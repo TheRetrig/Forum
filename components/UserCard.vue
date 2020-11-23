f4b613cabc4ba90a519d9fe35cf31f457452a187
auralshin <anshulspartan141@gmail.com>
2020-11-23 20:46:17 +0530

    complete

---
+++ components/UserCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="elevation-0">
+    <p class="card-text"></p>
+    <div class="author">
+      <div class="block block-one"></div>
+      <div class="block block-two"></div>
+      <div class="block block-three"></div>
+      <div class="block block-four"></div>
+      <a href="javascript:void(0)">
+        <v-avatar height="200" width="200">
+        <img  src="https://cdn.vuetifyjs.com/images/john.jpg" alt="..." />
+        </v-avatar>
+        <h5 class="title">Mike Andrew</h5>
+      </a>
+      <p class="description">Ceo/Co-Founder</p>
+    </div>
+    <p></p>
+    <div class="card-description">
+      Do not be scared of the truth because we need to restart the human
+      foundation in truth And I love you like Kanye loves Kanye I love Rick
+      Owens’ bed design but the back is...
+    </div>
+
+    <div slot="footer" class="button-container">
+      <v-btn icon round>
+        <i class="fab fa-facebook"></i>
+      </v-btn>
+      <v-btn icon round>
+        <i class="fab fa-twitter"></i>
+      </v-btn>
+      <v-btn icon round>
+        <i class="fab fa-google-plus"></i>
+      </v-btn>
+    </div>
+  </v-card>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ pages/profile.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container class="pt-12">
+        <v-row>
+            <v-col cols="4">
+                <v-avatar>
+                    <img
+                        src="https://cdn.vuetifyjs.com/images/john.jpg"
+                        alt="John"
+                        height="100"
+                    >
+                    </v-avatar>
+            </v-col>
+            <v-col cols="8"></v-col>
+        </v-row>
+    </v-container>
+</template>
